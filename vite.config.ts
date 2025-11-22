9d6ab8950402ac8b7b5c63cc8ef43a45040a3232
wormholepage <licannan3@gmail.com>
2025-11-22 09:10:17 +0000

    chore: set vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  // 👇 这里很重要：必须是 /仓库名/
+  base: '/Rosen-Bridge/',
 });
